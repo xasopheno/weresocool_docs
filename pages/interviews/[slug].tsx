4b4a084e8d131a1507fcb8ac96201094cd91b9ff
Danny Meyer <danny@xasopheno.com>
2021-03-28 22:04:42 -0400

    working on brad interview.

--- pages/interviews/[slug].tsx
+++ pages/interviews/[slug].tsx
@@ -1,21 +1,22 @@
-import fs from "fs";
-import matter from "gray-matter";
-import hydrate from "next-mdx-remote/hydrate";
-import renderToString from "next-mdx-remote/render-to-string";
-import Head from "next/head";
-import path from "path";
-import CustomLink from "../../components/CustomLink";
-import { interviewFilePaths, INTERVIEWS_PATH } from "../../utils/mdxUtils";
-import { MdxRemote } from "next-mdx-remote/types";
-import { GetStaticPropsResult, GetStaticPropsContext } from "next";
-import { capitalize } from "../../utils/misc";
-import { useRouter } from "next/router";
-import styled from "styled-components";
+import fs from "fs"
+import matter from "gray-matter"
+import hydrate from "next-mdx-remote/hydrate"
+import renderToString from "next-mdx-remote/render-to-string"
+import Head from "next/head"
+import path from "path"
+import CustomLink from "../../components/CustomLink"
+import { interviewFilePaths, INTERVIEWS_PATH } from "../../utils/mdxUtils"
+import { MdxRemote } from "next-mdx-remote/types"
+import { GetStaticPropsResult, GetStaticPropsContext } from "next"
+import { capitalize } from "../../utils/misc"
+import { useRouter } from "next/router"
+import styled from "styled-components"
+import Image from "next/image"
 
 const GoldLink = styled.div`
   color: goldenrod;
   font-size: 1.25em;
-`;
+`
 
 // Custom components/renderers to pass to MDX.
 // Since the MDX files aren't loaded by webpack, they have no knowledge of how
@@ -24,21 +25,22 @@ const GoldLink = styled.div`
 
 const components = {
   a: CustomLink,
+  Image: Image,
   // It also works with dynamically-imported components, which is especially
   // useful for conditionally loading components for certain routes.
   // See the notes in README.md for more details.
   Head,
-};
+}
 
 type FrontMatter = {
-  [key: string]: any;
-};
+  [key: string]: any
+}
 
-type PostProps = { source: MdxRemote.Source; frontMatter: FrontMatter };
+type PostProps = { source: MdxRemote.Source; frontMatter: FrontMatter }
 
 export default function InterviewPage({ source, frontMatter }: PostProps) {
-  const content = hydrate(source, { components });
-  const router = useRouter();
+  const content = hydrate(source, { components })
+  const router = useRouter()
 
   return (
     <div>
@@ -53,7 +55,7 @@ export default function InterviewPage({ source, frontMatter }: PostProps) {
         {frontMatter.next && (
           <GoldLink
             onClick={async () => {
-              router.push(`/interviews/${frontMatter.next}`);
+              router.push(`/interviews/${frontMatter.next}`)
             }}
           >
             {`Next Tutorial ~> ${capitalize(frontMatter.next)}`}
@@ -61,24 +63,24 @@ export default function InterviewPage({ source, frontMatter }: PostProps) {
         )}
       </div>
     </div>
-  );
+  )
 }
 
 interface StaticPropsType {
-  source: MdxRemote.Source;
-  frontMatter: FrontMatter;
-  slug: string | string[];
+  source: MdxRemote.Source
+  frontMatter: FrontMatter
+  slug: string | string[]
 }
 
 export async function getStaticProps(
   context: GetStaticPropsContext
 ): Promise<GetStaticPropsResult<StaticPropsType>> {
-  const slug = context.params!.slug;
-  const interviewFilePath = path.join(INTERVIEWS_PATH, `${slug}.mdx`);
-  console.log(interviewFilePath);
-  const source = fs.readFileSync(interviewFilePath);
+  const slug = context.params!.slug
+  const interviewFilePath = path.join(INTERVIEWS_PATH, `${slug}.mdx`)
+  console.log(interviewFilePath)
+  const source = fs.readFileSync(interviewFilePath)
 
-  const { content, data } = matter(source);
+  const { content, data } = matter(source)
 
   const mdxSource = await renderToString(content, {
     components,
@@ -87,7 +89,7 @@ export async function getStaticProps(
       rehypePlugins: [],
     },
     scope: data,
-  });
+  })
 
   return {
     props: {
@@ -95,7 +97,7 @@ export async function getStaticProps(
       frontMatter: data,
       slug: slug!,
     },
-  };
+  }
 }
 
 export const getStaticPaths = async () => {
@@ -103,10 +105,10 @@ export const getStaticPaths = async () => {
     // Remove file extensions for page paths
     .map((path) => path.replace(/\.mdx?$/, ""))
     // Map the path into the static paths object required by Next.js
-    .map((slug) => ({ params: { slug } }));
+    .map((slug) => ({ params: { slug } }))
 
   return {
     paths,
     fallback: false,
-  };
-};
+  }
+}
