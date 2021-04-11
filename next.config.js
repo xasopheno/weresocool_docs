f7fbb6270cc3ab7ec7e5dc6ca3d1b132071c2350
Danny Meyer <danny@xasopheno.com>
2021-04-10 22:20:30 -0400

    fix shared buffer array warning

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 module.exports = {
   webpack(config) {
-    config.output.webassemblyModuleFilename = "static/wasm/[modulehash].wasm";
-    return config;
+    config.output.webassemblyModuleFilename = "static/wasm/[modulehash].wasm"
+    return config
   },
-};
+  async headers() {
+    return [
+      {
+        source: "/:path*{/}?",
+        headers: [
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+        ],
+      },
+    ]
+  },
+}
