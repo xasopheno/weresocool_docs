856bb1f08d5e61d67c0144075ce3aad3ac04ec6f
Danny Meyer <danny@xasopheno.com>
2021-04-11 20:19:00 -0400

    wip

--- next.config.js
+++ next.config.js
@@ -3,44 +3,44 @@ module.exports = {
     config.output.webassemblyModuleFilename = "static/wasm/[modulehash].wasm"
     return config
   },
-  target: "serverless",
-  async rewrites() {
-    return [
-      // Rewrite everything to `pages/index`
-      {
-        source: "/:any*",
-        destination: "/",
-      },
-    ]
-  },
   async headers() {
     return [
       {
-        source: "/tutorials/:path*{/}?",
+        source: "/:path*{/}?",
         headers: [
           {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
-          },
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin",
+            key: "Origin-Trial",
+            value:
+              "AuzejKtwfDEPCLoYvY1PzI439qF1qRLhttXBYEmYNWK6QBsc6eDm0Uyhl+CRJVRDpnBhEROC+z7PLxpAdJqlkwwAAAB8eyJvcmlnaW4iOiJodHRwczovL3d3dy53ZXJlc29jb29sLm9yZzo0NDMiLCJmZWF0dXJlIjoiVW5yZXN0cmljdGVkU2hhcmVkQXJyYXlCdWZmZXIiLCJleHBpcnkiOjE2MzM0NzgzOTksImlzU3ViZG9tYWluIjp0cnVlfQ==",
           },
         ],
       },
-      {
-        source: "/tv/:path*{/}?",
-        headers: [
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "unsafe-none",
-          },
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "unsafe-none",
-          },
-        ],
-      },
+      //  {
+      //  source: "/tutorials/:path*{/}?",
+      //  headers: [
+      //  {
+      //  key: "Cross-Origin-Embedder-Policy",
+      //  value: "require-corp",
+      //  },
+      //  {
+      //  key: "Cross-Origin-Opener-Policy",
+      //  value: "same-origin",
+      //  },
+      //  ],
+      //  },
+      //  {
+      //  source: "/tv/:path*{/}?",
+      //  headers: [
+      //  {
+      //  key: "Cross-Origin-Embedder-Policy",
+      //  value: "unsafe-none",
+      //  },
+      //  {
+      //  key: "Cross-Origin-Opener-Policy",
+      //  value: "unsafe-none",
+      //  },
+      //  ],
+      //  },
     ]
   },
 }
