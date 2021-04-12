b968765feda4177e66519287ed02f5218c59ab70
Danny Meyer <danny@xasopheno.com>
2021-04-11 23:31:21 -0400

    mobile not bad

--- components/menu/Menu/MobileMenu.styled.ts
+++ components/menu/Menu/MobileMenu.styled.ts
@@ -36,11 +36,6 @@ export const MenuItem = styled.div`
   text-decoration: none;
   transition: color 0.3s linear;
 
-  //  @media (max-width: ${({ theme }) => theme.mobile}) {
-  //  font-size: 1.5rem;
-  //  text-align: center;
-  //  }
-
   &:hover {
     //color: ${({ theme }) => theme.primaryHover};
     color: #beb;
