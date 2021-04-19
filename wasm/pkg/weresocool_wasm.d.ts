8702c82e3bd9a6e5335b6cdfc943100c64fb0e4d
Danny Meyer <danny@xasopheno.com>
2021-04-19 00:16:06 -0400

    adds wasm pkg

---
+++ wasm/pkg/weresocool_wasm.d.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @returns {Manager}
+*/
+export function beep(): Manager;
+/**
+*/
+export function main_js(): void;
+/**
+*/
+export class Manager {
+  free(): void;
+/**
+* @param {string} language
+*/
+  push(language: string): void;
+/**
+* @param {number} value
+*/
+  update_volume(value: number): void;
+}
---
+++ wasm/pkg/weresocool_wasm_bg.wasm.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_manager_free(a: number): void;
+export function manager_push(a: number, b: number, c: number): void;
+export function manager_update_volume(a: number, b: number): void;
+export function beep(): number;
+export function main_js(): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h44a319c223e48609(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_start(): void;
