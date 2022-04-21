c62acf632e7d396d5b1d08ee04ccc9c094df02e2
xasopheno <danny@xasopheno.com>
2022-04-20 23:31:27 -0400

    WIP

--- utils/misc.ts
+++ utils/misc.ts
@@ -1,45 +1,45 @@
-import { useLoadedWasm } from "./useWasm"
+import { useLoadedWasm } from './useWasm';
 
 export const capitalize = (s: string): string => {
   return s
-    .split("_")
+    .split('_')
     .map((word, i) => {
-      if (i > 0 && ["and", "or", "of"].includes(word)) {
-        return word
+      if (i > 0 && ['and', 'or', 'of'].includes(word)) {
+        return word;
       } else {
-        return word.charAt(0).toUpperCase() + word.slice(1)
+        return word.charAt(0).toUpperCase() + word.slice(1);
       }
     })
-    .join(" ")
-}
+    .join(' ');
+};
 
 export const isMobile = () =>
-  window.matchMedia("only screen and (max-width: 760px)").matches
+  window.matchMedia('only screen and (max-width: 760px)').matches;
 
 export function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms))
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-export const stopLang = `{ f: 220, l: 1, g: 1, p: 0 }\nmain = {Fm 0}`
+export const stopLang = `{ f: 220, l: 1, g: 1, p: 0 }\nmain = {Fm 0}`;
 
 export const supportsWasm = () => {
   return (
-    typeof window !== "undefined" &&
-    (navigator.userAgent.indexOf("Chrome") != -1 ||
-      navigator.userAgent.indexOf("Firefox") != -1)
-  )
-}
+    typeof window !== 'undefined' &&
+    (navigator.userAgent.indexOf('Chrome') != -1 ||
+      navigator.userAgent.indexOf('Firefox') != -1)
+  );
+};
 
 export function useStopAndWait() {
   if (!supportsWasm()) {
-    return () => {}
+    return () => {};
   }
-  const { manager, readyState } = useLoadedWasm()
+  const { manager, readyState } = useLoadedWasm();
   const stopAndWait = async () => {
     if (manager && readyState === 1) {
-      manager.push(stopLang)
-      await sleep(150)
+      manager.push(stopLang);
+      await sleep(150);
     }
-  }
-  return stopAndWait
+  };
+  return stopAndWait;
 }
